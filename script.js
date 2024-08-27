5c8f772f4246f780a08411f79227f6f93a0da6e8
Yash <yashnigam1801@gmail.com>
2024-08-27 21:04:29 +0530

    toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleThemeButton = document.getElementById('toggleTheme');
+const body = document.body;
+
+toggleThemeButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
